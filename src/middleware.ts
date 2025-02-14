f8cef539a6bba7a891a0629c0265f1eac83319ff
MARGAM VINAY <margamvinay77@gmail.com>
2025-02-14 23:57:16 +0530

    fix:deployment error

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,17 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const token = req.cookies.get("token")?.value;
-  if (pathname === "/login" || pathname === "/signup") {
-    return NextResponse.next();
-  }
-  if (pathname === "/logout") {
-    req.cookies.set("token", "");
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // if (pathname === "/login" || pathname === "/signup") {
+  //   return NextResponse.next();
+  // }
+  // if (pathname === "/logout") {
+  //   req.cookies.set("token", "");
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 
   return NextResponse.next();
 }
